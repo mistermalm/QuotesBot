3551f75299775151739983ff2a7114524e74991e
MarcusAsp <mackan.a.b@hotmail.com>
2021-01-26 22:50:31 +0100

    c

--- bot.js
+++ bot.js
@@ -1,25 +1,32 @@
-import Discord from 'discord.js'
-import env from 'dotenv'
+const env = require( 'dotenv' )
+const Discord = require( 'discord.js' )
 env.config()
 
 const client = new Discord.Client()
-// const guild = new Discord.Guild()
+//const guild = new Discord.Guild()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`)
-  // let members = guild.members.cache.forEach((m) => console.log(m))
-  let guild = client.guilds.cache.get(process.env.GUILD_ID)
-  // guild.members.fetch({ force: true })
-  console.log(guild.members)
-  // guild.members.fetch()
+client.on( 'ready', async () => {
+  console.log( `Logged in as ${client.user.tag}` )
+  let guild = client.guilds.cache.get( process.env.GUILD_ID )
+  let members = await guild.members.fetch()
+  
+  let patten = members.filter( elem => elem.user.username == "Pstanizewski" )
+
+  let patteMember
+  patten.forEach( p => patteMember = p )
+
+  let kickPatten = new Discord.GuildMember( client, patteMember, guild );
+  // console.log(kickPatten)
+  kickPatten.kick()
+
 })
 
-client.on('message', (msg) => {
-  if (msg.author.bot) return false
-  if (msg.content === 'ping') {
-    msg.channel.send('pong')
+client.on( 'message', ( msg ) => {
+  if ( msg.author.bot ) return false
+  if ( msg.content === 'ping' ) {
+    msg.channel.send( 'pong' )
   }
 })
 
 // client.guilds.map((i) => console.log(i))
-client.login(process.env.BOT_TOKEN)
+client.login( process.env.BOT_TOKEN )
